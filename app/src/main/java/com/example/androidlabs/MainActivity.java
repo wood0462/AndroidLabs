cec3a72d8043a4ec5a3a18c450ddc51e5f76d08b
Mack <wood0462@algonquinlive.com>
2019-01-31 15:49:20 -0500

    Finished Lab3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,15 +1,53 @@
 package com.example.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences sp;
+    EditText type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_lab3);
+
+       type = findViewById(R.id.email);
+        sp = getSharedPreferences("pref", Context.MODE_PRIVATE);
+        String sa = sp.getString("email", "");
+
+        type.setText(sa);
+
+        findViewById(R.id.butt).setOnClickListener( b -> {
+
+            Intent in = new Intent(this, ProfileActivity.class);
+            startActivity(in.putExtra("email", type.getText().toString()));
+
+
+
+        });
+
+
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+            SharedPreferences.Editor spe = sp.edit();
+            String tp = type.getText().toString();
+            spe.putString("email", tp);
+
+            spe.commit();
+
+
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,75 @@
+package com.example.androidlabs;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+    EditText type;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+    ImageButton mImageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.e(ACTIVITY_NAME, "onCreate: Error");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Intent in = getIntent();
+
+        String em = in.getStringExtra("email");
+        type = findViewById(R.id.email);
+        type.setText(em);
+
+        mImageButton = findViewById(R.id.picture);
+        findViewById(R.id.picture).setOnClickListener( b -> {
+            dispatchTakePictureIntent();
+
+        });
+
+        Log.e(ACTIVITY_NAME, "onCreate: Error");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "onPause: Error");
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+            Log.e(ACTIVITY_NAME, "onActivityResult: Error");
+
+
+
+        }
+    }
+
+    protected void OnDestroy(){
+        super.onDestroy();
+
+
+
+    }
+
+
+
+}
